44b2d3093c15d0d5b31e35846d926c80d3f2b105
Soundaryasakthi <soundaryasakthi14@gmail.com>
2017-04-15 13:07:22 +0530

    Create armstrongnumber.c

---
+++ armstrongnumber.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int x,n,y=0,t;
+printf("Enter the number\n");
+scanf("%d",&n);
+t=n;
+while(n>0)
+{
+x=n%10;
+y=y+x*x*x*;
+n=n/10;
+}
+if(y==t)
+{
+printf("Armstrong Number\n");
+}
+else
+{
+printf("Not a Armstrong Number\n");
+}
+return 0;
+}
